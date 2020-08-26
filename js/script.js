b6472ced18e88eb8909cd40182fa4adc80938511
Jean Carlos <jeancarlosquejadatoro@gmail.com>
2020-08-26 07:42:12 -0500

    Actividad

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 //Código de lógica
+/*3) Obtener datos de la siguiente URL https://developer.marvel.com/documentation/getting_started 
+mediante (requiere registro y autenticación mediante API Key) fetch y 
+crear de forma dinámica componentes card (uno por cada a posición o registro de la estructura de datos obtenida)
+ https://getbootstrap.com/docs/4.5/components/card/.
+ 
+*/
