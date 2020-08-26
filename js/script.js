16b44122716a45702010f2bc90a49e667de1bd19
Jean Carlos <jeancarlosquejadatoro@gmail.com>
2020-08-26 07:46:27 -0500

    Subo la estructira de la Actividad 2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-//Código de valentina, aquí vas hacer uso de la Api-rest de json
+//Lógica
+/*2) Obtener datos de la siguiente URL https://restcountries.eu/rest/v2/regionalbloc/USAN 
+mediante fetch y crear de forma dinámica un elemento select con sus options => uno por cada a posición o registro 
+de la estructura de datos obtenida, https://developer.mozilla.org/es/docs/Web/HTML/Elemento/select.
+*/
