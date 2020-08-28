b3cfc40d9ec41207bcf8bb6a1441b8bc1cc61029
1JuanM2 <juanmontoya1038@gmail.com>
2020-08-28 09:10:15 -0500

    subo el punto 1 con jumbotron

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-//Codigo de Federico
+const tabla = document.querySelector('.container table tbody');
+
+function cargarUsuarios(){
+    fetch('usuarios.json')
+        .then(respuesta => respuesta.json()) //Indicamos el formato en que se desea obtener la información
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                const row = document.createElement('tr');
+                row.innerHTML += `
+                    <td>${usuario.postId}</td>
+                    <td>${usuario.id}</td>
+                    <td>${usuario.name}</td>
+                    <td>${usuario.email}</td>
+                    <td>${usuario.body}</td>
+                `;
+                console.log(tabla);
+                tabla.appendChild(row);                
+            });
+        }) // Aquí mostramos dicha información
+        //.catch(error => console.log('Hubo un error : ' + error.message))
+}
+
+cargarUsuarios();
